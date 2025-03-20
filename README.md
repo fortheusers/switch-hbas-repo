ac5918bf33f354bd3ed3eca670634c324ff62f07
vgmoose <me@vgmoose.com>
2025-03-20 14:07:09 -0400

    update McOsu, SRB2Kart, and TotKUnexplored, add stage_update script, update readme

--- README.md
+++ README.md
@@ -3,5 +3,20 @@ This git repository contains package metadata that generates the libget repo pub
 
 The syntax of the pkgbuild.json files here are defined in the [spinarak](https://github.com/fortheusers/spinarak) project README.
 
+If a package folder is missing here, but you can see it on [hb-app.store](https://hb-app.store), then it's being maintained manually on our secondary repo. In this case, the `stage_update.py` script (see below) can be used to help migrate existing package data to this files in this repo.
+
+The [Magnezone](https://github.com/fortheusers/magnezone) README details further how this primary/secondary repo sourcing works. Our long term goal though is to have all packages going forward be managed by pkgbuild metadata in this repo.
+
 ### Contributing
+If you want to add a new, or update an existing app, please feel free to open a [Pull request](https://github.com/fortheusers/switch-hbas-repo/pulls) with your changes!
+
 The metadata within this repo is available to use under a [CC-BY-SA license](https://creativecommons.org/licenses/by-sa/4.0/deed.en).
+
+### pkgbuild info
+The `assets` section of the pkgbuild.json contains info about how to source the files that will end up in the zip, as well as to which paths they will be downloaded to. This allows for a mapping of URLs to specific folders on the SD card, after downloading.
+
+For an explanation of the different extraction states (Install, Extract, Update, Get), see the [libget Wiki](https://github.com/fortheusers/libget/wiki/Overview-&-Glossary). Concisely, most files will use the `update` state, which indicates that the file should always be extracted on update or install of the package.
+
+See the [Spinarak Wiki](https://gitlab.com/4TU/spinarak/-/wikis/pkgbuild.json-specification) for more detailed information on pkgbuild keys and assets. If the `version` key is bumped, the package will be rebuilt by the CI, including the re-sourcing and re-downloading of any required assets.
+
+For migrating an existing package into a pkgbuild, you can run `python3 stage_update.py <package name>` and a template directory, pkgbuild.json, and assets will be created. It includes both an example for extracting a zip, or a simple one-file download.
---
+++ stage_update.py
@@ -0,0 +1,101 @@
+#!/usr/local/env python3
+# This is a helper script to create a new package folder with a default pkgbuild.json,
+# with pre-populated fields from the current live repo.json file.
+
+import requests
+import json
+import os, sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 stage_update.py <package_name>")
+    sys.exit(1)
+
+package = sys.argv[1]
+
+# download the current repo
+REPO = "https://switch.cdn.fortheusers.org"
+response = requests.get(f"{REPO}/repo.json")
+if response.status_code != 200:
+    print("Failed to download repo.json")
+    sys.exit(1)
+repo = response.json()
+
+# get the existing libget package data from the repo
+data = [pkg for pkg in repo["packages"] if pkg["name"] == package][0]
+if data is None:
+    print("Package not found in existing repo.json")
+    sys.exit(1)
+
+# create the new package folder
+os.makedirs(f"packages/{package}", exist_ok=True)
+
+# TODO: if we have some pkgbuild.json data already, persist it here for convenience
+
+# build the template pkgbuild.json
+template = {
+    "package": package,
+    "info": {
+        "title": "",
+        "author": "",
+        "category": "",
+        "version": "",
+        "url": "",
+        "license": "",
+        "description": "",
+        "details": "",
+    },
+    "changelog": "",
+    "assets": [],
+}
+
+# for each key in the template, copy the data from the repo
+for key in template["info"]:
+    if key in data:
+        template["info"][key] = data[key]
+if "changelog" in data:
+    template["changelog"] = data["changelog"]
+
+# create the initial assets, assuming a simple file, or simple extract
+template["assets"].append({
+    "url": "https://example.com/file.zip",
+    "type": "zip",
+    "zip": [{
+        "path": "/**",
+        "dest": "/",
+        "type": "update",
+    }]
+})
+
+template["assets"].append({
+    "url": "https://example.com/file.nro",
+    "dest": data["binary"] if "binary" in data else "/switch/path/etc.nro",
+    "type": "update",
+})
+
+# try to download the assets and any screen shots
+screensCount = data["screens"]
+screenSlugs = [f"screen{i}" for i in range(1, screensCount + 1)]
+dlUrls = [f"{REPO}/packages/{package}/{name}.png" for name in ["icon", "screen"] + screenSlugs]
+
+for url in dlUrls:
+    response = requests.get(url)
+    if response.status_code == 200:
+        fileName = os.path.basename(url)
+        with open(f"packages/{package}/{fileName}", "wb") as f:
+            f.write(response.content)
+            assetType = "screenshot"
+            if fileName == "icon.png":
+                assetType = "icon"
+            if fileName == "screen.png":
+                assetType = "banner"
+            # wrote the file, update our template assets
+            template["assets"].append({
+                "type": assetType,
+                "url": fileName,
+            })
+
+# write the template to the new package folder
+with open(f"packages/{package}/pkgbuild.json", "w") as f:
+    json.dump(template, f, indent=4)
+
+print(f"Created new package folder for {package} in packages")
